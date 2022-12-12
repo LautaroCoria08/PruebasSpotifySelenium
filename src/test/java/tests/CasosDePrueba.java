306eedb1b76a5196c289a1435e269be8960cad4c
LautaroCoria08 <lautaro.coria@tsoftglobal.com>
2022-12-12 20:22:03 -0300

    2 pruebas

--- src/test/java/tests/CasosDePrueba.java
+++ src/test/java/tests/CasosDePrueba.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -139,18 +140,22 @@ public class CasosDePrueba {
 
         Assert.assertEquals(driver.findElement(By.xpath("//div[contains(text(),'Las direcciones de correo')]")).getText(),"Las direcciones de correo electrónico no coinciden.");
     }
-    @Test
-    public void CP003_Inicio_sesion_fallido() {
+   @Test
+    public void CP003_Inicio_Sesion_Apple() throws InterruptedException {
 
-        By localizadorBtnIniciarSesion = By.xpath("//span[contains(text(),'Iniciar sesión')]");
-        WebElement BtnIniciarSesion = driver.findElement(localizadorBtnIniciarSesion);
+        //By localizadorBtnIniciaSeccion = By.xpath("//span[text() = 'Iniciar sesión']");
+        @FindBy(xpath = "//[contains(@data-testid='apple-login')]");
 
-        BtnIniciarSesion.click();
+       // WebElement btnInicioSesion = driver.findElement(localizadorBtnIniciaSeccion);
+
+        WebElement textoIniciarsesion;
+
+        textoIniciarsesion.click();
+
+        driver.findElement(By.xpath("//button[@data-testid= 'apple-login']")).click();
 
 
-       //driver.findElement(By.xpath("//*[@id=\"login-username\"]")).sendKeys("lautaro.coria");
-
-        //WebElement btnInicio  = driver.findElement(By.xpath("//*[@id=\"login-button\"]/div[1]/p"));//
+        Assert.assertEquals(driver.getTitle(),"Inicia sesión con el ID de Apple");
 
     }
 
@@ -158,7 +163,7 @@ public class CasosDePrueba {
     public void CP004_Registro_Fallido_Mes() throws InterruptedException {
         System.setProperty(propertyDriver,rutaDriver);
 
-        By localizadorBtnRegistrarse = By.xpath("//button[contains(text(),'Regístrate')]");
+        By localizadorBtnRegistrarse = By.xpath("//button[contains(text(),'Regístrarte')]");
 
         WebElement btnRegistrarse = driver.findElement(localizadorBtnRegistrarse);
 
@@ -217,6 +222,63 @@ public class CasosDePrueba {
 
     }
 
+    @Test
+    public void CP006_Inicio_Sesion_con_Facebook() {
+
+        By localizadorBtnIniciarSesion = By.xpath("//span[contains(text(),'Iniciar sesión')]");
+
+        WebElement btnIniciarSesion = driver.findElement(localizadorBtnIniciarSesion);
+
+        btnIniciarSesion.click();
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//button[@data-testid='facebook-login']"))).click();
+
+        Assert.assertEquals(driver.getTitle(),"Iniciar sesión en Facebook | Facebook");
+
+    }
+    @Test
+    public void CP007_Contraseña_Corta() {
+
+        By localizadorBtnRegistrase = By.xpath("//button[contains(text(),'Registrarte')]");
+
+        WebElement btnRegistrarse = driver.findElement(localizadorBtnRegistrase);
+
+        btnRegistrarse.click();
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("email"))).sendKeys("lautaro.coria@tsoftglobal.com");
+
+        driver.findElement(By.name("confirm")).sendKeys("lautaro.coria@tsoftglobal.com");
+
+        driver.findElement(By.name("password")).sendKeys("1234");
+
+        driver.findElement(By.name("displayname")).sendKeys("Lautaro");
+
+        driver.findElement(By.id("day")).sendKeys("17");
+
+        Select cmbMes = new Select(driver.findElement(By.id("month")));
+
+        cmbMes.selectByValue("10");
+
+        driver.findElement(By.name("year")).sendKeys("1998");
+
+        WebElement optionMale = driver.findElement(By.xpath("//label[@for='gender_option_male']"));
+
+        js.executeScript("arguments[0].scrollIntoView();", optionMale);
+
+        optionMale.click();
+
+        driver.findElement(By.xpath("//label[@for='marketing-opt-checkbox']")).click();
+
+        driver.findElement(By.xpath("//label[@for='third-party-checkbox']")).click();
+
+        WebElement btnRegistro  = driver.findElement(By.xpath("//button[@type='submit']"));
+
+        js.executeScript("arguments[0].scrollIntoView();", btnRegistro);
+
+        btnRegistro.click();
+
+        Assert.assertEquals(driver.findElement(By.xpath("//div[contains(text(),'Tu contraseña es')]")).getText(),"Tu contraseña es demasiado corta.");
+    }
 
 
 
